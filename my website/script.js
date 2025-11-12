0c97643cc09df675040ad15d0e40c19cc65e0b47
bvella9oct-lab <bvella9oct@gmail.com>
2025-11-12 23:14:15 +0500

    Add files via upload

---
+++ my website/script.js
@@ -0,0 +1,9 @@
+// Simple scroll animation
+window.addEventListener('scroll', () => {
+  document.querySelectorAll('section').forEach(sec => {
+    const rect = sec.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      sec.classList.add('visible');
+    }
+  });
+});
